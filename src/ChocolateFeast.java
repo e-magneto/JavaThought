84b84443919f870cac4a42aa29b6559fbb74aa26
Sutanu <ansi.d@outlook.com>
2015-05-13 21:17:40 +0530

    Chocolate Feast problem solution

---
+++ src/ChocolateFeast.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ChocolateFeast {
+	
+	public static long chocoCount(int N, int C, int M){
+		int finalcount=0; 
+		int count =0;
+		if(N>C){
+			finalcount = N/C;
+			int tempcount=finalcount;
+			count= finalcount;
+			while(tempcount>=M){
+				count +=tempcount/M;
+				tempcount= tempcount/M+tempcount%M;
+			}
+		}
+		return (long)count;
+	}
+	
+	@SuppressWarnings("resource")
+	public static void main(String [] args){
+		
+		Scanner in = new Scanner(System.in);
+		int numberOfTests= in.nextInt();
+		for(int i=0; i<numberOfTests; i++){
+			int money= (int)in.nextInt();
+			int price= (int)in.nextInt();
+			int wrapper= (int)in.nextInt();
+			long answer=chocoCount(money, price, wrapper);
+			System.out.println(answer);
+		}
+	}
+
+}
